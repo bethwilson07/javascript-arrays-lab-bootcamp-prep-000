c72eec82b57998a40318e4bed1c84b3d784022ef
Beth Wilson <beth.ny.wlsn@gmail.com>
2018-07-26 20:19:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your functions and code here
+kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  destructivelyAppendKitten.push(name);
+  return destructivelyAppendKitten;
+}
+
+function destructivelyPrependKitten(name) {
+  destructivelyPrependKitten.unshift(name);
+  return destructivelyPrependKitten;
+}
+
+function destructivelyRemoveLastKitten() {
+  destructivelyRemoveLastKitten.pop();
+  return destructivelyRemoveLastKitten;
+}
+
+function destructivelyRemoveFirstKitten() {
+  destructivelyRemoveFirstKitten.shift();
+  return destructivelyRemoveFirstKitten;
+}
+
+function appendKitten(name) {
+  
+}
